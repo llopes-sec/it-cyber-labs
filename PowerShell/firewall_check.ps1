98c71ada10ff2d41bad804b913a47dbd8480a547
Lucas Lopes <llopes.sec@gmail.com>
2026-03-20 17:05:51 -0300

    Update firewall_check.ps1

--- PowerShell/firewall_check.ps1
+++ PowerShell/firewall_check.ps1
@@ -1,3 +1,8 @@
+# Script: firewall_check.ps1
+# Descrição: verifica o status dos perfis de firewall do Windows
+# Objetivo: validar se o firewall está ativo nos diferentes perfis
+# Autor: Lucas
+
 $firewall = Get-NetFirewallProfile | Select-Object Name, Enabled
 
 foreach ($f in $firewall) {
